6239926c03537b309d3df55da305561cd842edad
Danilo Dorgam <danilodorgam@gmail.com>
2023-01-27 17:09:59 -0300

    criacao de funcoes tocaSomPom e tocaSomClap

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom(){
+    document.querySelector('#som_tecla_pom').play();
+}
+
+function tocaSomClap() {
+    document.querySelector('#som_tecla_clap').play();    
+}
