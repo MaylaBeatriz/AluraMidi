0aa4ca8950992820f24e186f836aba190372e60f
Mayla Rocha <maylaetb@gmail.com>
2023-01-31 11:35:35 -0300

    mapeando tecla espaco

--- main.js
+++ main.js
@@ -10,5 +10,16 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;  //template string
-    tecla.onclick = function (){ tocaSom(idAudio) };
+
+    tecla.onclick = function (){ 
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space'){
+            tecla.classList.add('ativa');
+        }
+    }
+    
 }
+
